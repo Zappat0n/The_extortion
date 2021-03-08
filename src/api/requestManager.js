618a09d0792bd7f834fded2996390ec5ed9009de
Zappat0n <albarros1973@gmail.com>
2021-03-08 13:39:12 +0100

    Move requests to async blocks

--- src/api/requestManager.js
+++ src/api/requestManager.js
@@ -9,27 +9,29 @@ const requests = {
     document.querySelector('body').appendChild(div);
     return content;
   },
-  getScores() {
-    const request = getScoresRequest();
-    return Promise.resolve(
-      fetch(request)
-        .then(response => response.json())
-        .then((response) => response)
-        .catch((error) => this.displayError(error)),
-    );
+  async getScores() {
+    try {
+      const request = getScoresRequest();
+      const response = await fetch(request);
+      return response.json();
+    } catch (error) {
+      this.displayError(error);
+      return null;
+    }
   },
-  setScore(element) {
-    const data = {
-      user: element.user,
-      score: element.score,
-    };
-    const request = setScoreRequest(data);
-    return Promise.resolve(
-      fetch(request)
-        .then((response) => response.json())
-        .then((response) => response)
-        .catch((error) => this.displayError(error)),
-    );
+  async setScore(element) {
+    try {
+      const data = {
+        user: element.user,
+        score: element.score,
+      };
+      const request = setScoreRequest(data);
+      const response = await fetch(request);
+      return response.json();
+    } catch (error) {
+      this.displayError(error);
+      return null;
+    }
   },
 };
 
